342f6826d53dc7308e0d8f2e42efc089bd8aef45
brixzen <brixzen@hotmail.com>
2020-03-29 04:48:25 -0400

    use of argv

--- bogoSort.c
+++ bogoSort.c
@@ -4,6 +4,7 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <string.h>
 
 
 /*
@@ -11,20 +12,16 @@ Use: Enter data through console
 Create function and makefile
 */
 
-void shuffleMessage(){
-  printf("\nGetting ready to shuffle..");
-  sleep(2);
-  printf(".");
-  sleep(2);
-  printf(".\n");
-}
 
-void shuffleAction(int elements[]){
-  int takenSlots[8];
+void shuffleAction(int size, char *elements[]){
+  int takenSlots[size];
   int shuffledCard;
-  int holder;
-  for(int n=0;n<8;n++){
-    shuffledCard = rand() % 8;
+  char *holder;
+  for(int n=1;n<size;n++){
+    shuffledCard = rand() % size;
+    if(shuffledCard==0){
+      shuffledCard = 1;
+    }
     if(takenSlots[shuffledCard] != 1){
       holder = elements[n];
       elements[n] = elements[shuffledCard];
@@ -32,7 +29,7 @@ void shuffleAction(int elements[]){
       takenSlots[shuffledCard] = 1;
       takenSlots[n] = 1;
     }else{
-      for(int z=0;z<8;z++){
+      for(int z=1;z<size;z++){
         if(takenSlots[z]!=1){
           holder = elements[n];
           elements[n] = elements[shuffledCard];
@@ -45,29 +42,13 @@ void shuffleAction(int elements[]){
     }
   }
 }
-/*
-//Return 0 for unsorted and 1 for sorted
-//O(N)
-int checkSort(int elements[]){
-  int sortCheck = 0;
-  int lowest = elements[0];
-  int k = 0;
-  int breaky = 0;
-  for(int n=0;n<4;n++){
-    if(elements[n]>elements[n+1]){
-      printf("%d is greater than %d", elements[n], elements[n+1]);
-      return 0;
-    }
-  }
-return 1;
-}*/
 //O(N)
 //Returns 0 if unsorted, 1 if sorted
 int checkSort(int size, char *elements[]){
   int n;
-  for(n=0;n<size-1;n++){
+  for(n=1;n<size-1;n++){
     if(atoi(elements[n])>atoi(elements[n+1])){
-      printf("%d is greater than %d", atoi(elements[n]), atoi(elements[n+1]));
+      //printf("%d is greater than %d", atoi(elements[n]), atoi(elements[n+1]));
       return 0;
     }
   }
@@ -84,34 +65,36 @@ int main(int argc, char *argv[]){
     int elements2[5] = {1,2,3,4,5};
     int iterations = 0;
 
+    /*
     //Populate array
     for(int n=0;n<8;n++){
       elements[n] = rand() % 100;
-    }
-    printf("Elements unsorted:        ");
-    for(int n=0;n<8;n++){
-      printf("%d ", elements[n]);
-    }
-
-    shuffleAction(elements);
-    printf("\nElements after shuffle:   ");
-    for(int n=0;n<8;n++){
-      printf("%d ", elements[n]);
+    }*/
+    printf("Elements in entered order:      ");
+    for(int n=1;n<argc;n++){
+      printf("%s ", argv[n]);
     }
     printf("\n");
+/*
+    shuffleAction(argc, argv);
+    printf("\nElements after shuffle:   ");
+    for(int n=1;n<argc;n++){
+      printf("%s ", argv[n]);
+    }
+    printf("\n");*/
 
     while(checkSort(argc, argv) == 0){
 
-      printf("\nElements unsorted:        ");
-      for(int n=0;n<8;n++){
-        printf("%d ", elements[n]);
+      printf("\nElements after shuffle:         ");
+      for(int n=1;n<argc;n++){
+        printf("%s ", argv[n]);
       }
-      shuffleAction(elements);
+      shuffleAction(argc, argv);
       iterations++;
     }
-    printf("\nElements sorted:          ");
-    for(int n=0;n<8;n++){
-      printf("%d ", elements[n]);
+    printf("\n\nElements sorted:                ");
+    for(int n=1;n<argc;n++){
+      printf("%s ", argv[n]);
     }
     printf("\n%d number of shuffles required\n", iterations);
     return 0;
